dd325273f70332b856afbc6668d81cf461b0e4f7
Daniel Kipp <daniel.kipp@gmail.com>
2024-06-12 10:41:27 +0200

    created src/lib.rs to and moved Config and run to it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     // to collect arguments from std:in
@@ -15,36 +15,10 @@ fn main() {
     });
     println!("query: {}, file path: {}", config.query, config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("An error occured: {}", e);
 
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // read file to search through to string
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("text:\n{contents}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // expected arguments: 2
-        if args.len() < 3 {
-            return Err("Not enough arguments!\nSyntax: minigrep <search term> <file path>");
-        }
-        // clone to avoid lifetime handeling -> improve later!!
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
