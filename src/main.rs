29b5bcd7d5f58588a8963a7cee8e1939f004c332
Daniel Kipp <daniel.kipp@gmail.com>
2024-06-12 08:39:29 +0200

    implemented basic read functionality - read args from std:in and read file from path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // to collect arguments from std:in
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+    let query = &args[1];
+    let file_path = &args[2];
+
+    // println!("query: {}, file path: {}", query, file_path);
+    let contents = fs::read_to_string(file_path)
+        .expect("something went wrong reading the file");
+    // println!("text:\n{contents}");
 }
