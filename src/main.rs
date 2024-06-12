e196abcee524832f0ebda6423b5c222bade07e60
Daniel Kipp <daniel.kipp@gmail.com>
2024-06-12 09:12:37 +0200

    refactored reading args logic in struct Config with #new

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,24 @@ use std::fs;
 fn main() {
     // to collect arguments from std:in
     let args: Vec<String> = env::args().collect();
-    // dbg!(args);
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
+    println!("query: {}, file path: {}", config.query, config.file_path);
 
-    // println!("query: {}, file path: {}", query, file_path);
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("something went wrong reading the file");
     // println!("text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
